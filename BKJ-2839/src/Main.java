b714df3443f4385fad0395b088f7f79b4658fd3d
Alonely Winter Night <tlqdnjsahwk@gmail.com>
2016-12-29 19:25:52 +0900

    BKJ-2839 설탕 봉지 수 계산기 // 수정

--- BKJ-2839/src/Main.java
+++ BKJ-2839/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 		int N = sc.nextInt(); // 배달해야하는 설탕의 무게 N 킬로그램
 		int bag_3 = 0;
 		int bag_5 = 0;
-
 		if (N >= 3 && N <= 5000) {
 			while (N != 0 && N > 0) {
 				if (N % 5 == 3 && N % 3 != 0 || N%5 == 0) {
