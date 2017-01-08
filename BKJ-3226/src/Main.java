5d0c4d0fd0074b91e3146c3df789900e26cb36ee
Alonely Winter Night <tlqdnjsahwk@gmail.com>
2017-01-08 15:16:18 +0900

    Solved.
    helped by solstar

--- BKJ-3226/src/Main.java
+++ BKJ-3226/src/Main.java
@@ -26,28 +26,24 @@ public class Main {
 	}
 
 	public static int CalFee(int HH, int MM, int DD) {
-		System.out.println("Total Fee for the Call which took at "+HH+":"+MM+" "+DD);
+		System.out.println("Total Fee for the Call which took at " + HH + ":" + MM + " " + DD);
 		int fee = 0;
-		for (int min = 0; min < DD; min++) {
-			if (MM + 1 == 60) { // 통화 시작시간에서 통화시간을 점차 더한 값이 60이 되면
-				MM = MM + 1 - 60; // 0으로 리셋.
-				if (HH + 1 == 24) {
-					HH = HH + 1 - 24;
-				} else {
-					HH = HH + 1; // 시간은 1 올림.
-				}
+		while(DD-- > 0){
+			if (HH >= 7 && HH <= 18) {
+				fee += 10;
 			} else {
-				MM = MM + 1; // 1분씩 증가.
+				fee += 5;
 			}
 
-			if ((HH >= 07 && HH < 19 && MM > 01) || (HH == 19 && MM == 00) || (HH == 07 && MM == 00)) {
-				fee += 10;
-				System.out.println("Fee : " + fee + " until " + HH + ":" + MM + " (" + (min + 1) + "min)");
-			} else if ((HH <= 07) || (HH >= 19 && HH <= 24) || (HH == 19 && MM == 00) || (HH == 07 && MM == 00)) {
-				// else if (HH >= 19 || (HH < 07 && MM < 60) || (HH == 07 && MM
-				// == 00)) {
-				fee += 5;
-				System.out.println("Fee : " + fee + " until " + HH + ":" + MM + " (" + (min + 1) + "min)");
+			if (MM + 1 == 60) { // 통화 시작시간에서 통화시간을 점차 더한 값이 60이 되면
+				MM = 0; // 0으로 리셋.
+				if (HH + 1 == 24) {
+					HH = 0;
+				} else {
+					HH += 1; // 시간은 1 올림.
+				}
+			} else {
+				MM += 1; // 1분씩 증가.
 			}
 		}
 		return fee;
@@ -55,22 +51,10 @@ public class Main {
 }
 
 /*
- * Test case : 1st - 150 correctly // 2nd - 225 correctly  
-1
+ * Test case : 1st - 150 correctly // 2nd - 225 correctly 
+1 
 18:55 25
+
 1
 06:55 25
- */
-
-/*
- * 
- * if (Integer.parseInt(arr[0]) <= 9) { HH[i][0] = 0; HH[i][1] =
- * Integer.parseInt(arr[0]); } else { String[] temp = arr[0].split(""); for (int
- * temp_i = 0; temp_i < temp.length; temp_i++) { HH[i][temp_i] =
- * Integer.parseInt(temp[temp_i]); } }
- * 
- * if (Integer.parseInt(arr[1]) <= 9) { MM[i][0] = 0; MM[i][1] =
- * Integer.parseInt(arr[1]); } else { String[] temp = arr[1].split(""); for (int
- * temp_i = 0; temp_i < temp.length; temp_i++) { MM[i][temp_i] =
- * Integer.parseInt(temp[temp_i]); } } DD[i] = Integer.parseInt(arr[2]);
  */
